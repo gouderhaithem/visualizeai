52bb475209568498288f40702600657aa1e6eb88
unknown <haithem.gouder@univ-constantine2.dz>
2024-12-02 09:40:47 +0100

    add webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/profile(.*)", "/forum(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
